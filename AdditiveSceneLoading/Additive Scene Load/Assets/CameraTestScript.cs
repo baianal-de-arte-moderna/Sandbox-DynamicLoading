1376f582b7687a9dc4abeb28038709673290fcc8
Piramba <avpaganini@gmail.com>
2019-04-11 22:26:26 -0300

    Druken Camera

---
+++ AdditiveSceneLoading/Additive Scene Load/Assets/CameraTestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTestScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // JUST DON'T
+        //Camera.main.orthographicSize = Mathf.Lerp(
+        //    Camera.main.orthographicSize,
+        //    4 + Random.value * 2,
+        //    0.1f
+        //);
+    }
+}
